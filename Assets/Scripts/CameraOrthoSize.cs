f41d43d56ee96b86642481db2ff48802d34f1479
unknown <andrew_nomie@msn.com>
2024-01-22 21:57:04 +0000

    Fix lines between tiles

--- Assets/Scripts/CameraOrthoSize.cs
+++ Assets/Scripts/CameraOrthoSize.cs
@@ -4,12 +4,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraOrthoSize : MonoBehaviour {
+    //[SerializeField]
+    //private float baseOrthoSize = 20;
 
     [SerializeField]
-    private float baseOrthoSize = 20;
+    private float PIXELS_PER_UNIT = 16;
+
+    [SerializeField]
+    private float PPU_SCALE = 2;
 
     private void Update() {
-        float orthoSize = baseOrthoSize * Screen.height / Screen.width * .5f;
+        //float orthoSize = baseOrthoSize * Screen.height / Screen.width * .5f;
+        float orthoSize = ((Screen.height) / (PPU_SCALE * PIXELS_PER_UNIT)) * 0.5f;
 
         GetComponent<CinemachineVirtualCamera>().m_Lens.OrthographicSize = orthoSize;
     }
