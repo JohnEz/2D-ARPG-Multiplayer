1052ade5c99ceee2b80ec26027cd7e307dceb36e
unknown <andrew_nomie@msn.com>
2024-01-26 12:20:16 +0000

    Add character flash

--- Assets/Scripts/FlashOnHit.cs
+++ Assets/Scripts/FlashOnHit.cs
@@ -1,27 +1,34 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 public class FlashOnHit : MonoBehaviour {
 
     [SerializeField]
-    private SpriteRenderer targetSpriteRenderer;
-
-    private Coroutine flashRoutine;
-
-    private Color originalColor;
+    private Color _flashColor = Color.white;
 
     [SerializeField]
-    private Color flashColor = Color.white;
+    private float _flashDuration = 0.25f;
 
-    private float flashDuration = 0.1f;
+    [SerializeField]
+    private AnimationCurve _flashSpeedCurve;
+
+    private List<SpriteRenderer> _spriteRenderers = new();
+    private List<Material> _materials = new();
+
+    private Coroutine _flashRoutine;
 
     private NetworkStats _myStats;
 
-    private void Start() {
-        originalColor = targetSpriteRenderer.color;
-
+    private void Awake() {
+        _spriteRenderers = GetComponentsInChildren<SpriteRenderer>().ToList();
         _myStats = GetComponent<NetworkStats>();
 
+        Init();
+    }
+
+    private void Start() {
         _myStats.OnTakeDamage += Flash;
     }
 
@@ -31,24 +38,46 @@ public class FlashOnHit : MonoBehaviour {
         }
     }
 
+    private void Init() {
+        _spriteRenderers.ForEach(spriteRenderer => {
+            _materials.Add(spriteRenderer.material);
+        });
+    }
+
     public void Flash(int damage, bool isShield, NetworkStats source) {
-        // If the flashRoutine is not null, then it is currently running.
-        if (flashRoutine != null) {
-            // In this case, we should stop it first.
-            // Multiple FlashRoutines the same time would cause bugs.
-            StopCoroutine(flashRoutine);
+        if (_flashRoutine != null) {
+            StopCoroutine(_flashRoutine);
         }
 
-        flashRoutine = StartCoroutine(FlashRoutine());
+        _flashRoutine = StartCoroutine(FlashRoutine());
     }
 
     private IEnumerator FlashRoutine() {
-        targetSpriteRenderer.color = flashColor;
+        SetFlashColor();
 
-        yield return new WaitForSeconds(flashDuration);
+        float currentFlashAmount = 0f;
+        float elapsedTime = 0f;
 
-        targetSpriteRenderer.color = originalColor;
+        while (elapsedTime < _flashDuration) {
+            elapsedTime += Time.deltaTime;
 
-        flashRoutine = null;
+            currentFlashAmount = Mathf.Lerp(1f, _flashSpeedCurve.Evaluate(elapsedTime), elapsedTime / _flashDuration);
+
+            SetFlashAmount(currentFlashAmount);
+
+            yield return null;
+        }
+    }
+
+    private void SetFlashColor() {
+        _materials.ForEach(material => {
+            material.SetColor("_FlashColor", _flashColor);
+        });
+    }
+
+    private void SetFlashAmount(float amount) {
+        _materials.ForEach(material => {
+            material.SetFloat("_FlashAmount", amount);
+        });
     }
 }
